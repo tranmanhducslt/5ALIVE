3f6d97b7c159b1d95500f1630820cf2b5fe5da6f
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2024-12-12 15:18:10 +0100

    separated classes - will continue later

---
+++ packDraw.java
@@ -0,0 +1,61 @@
+import card;
+public static class pack{ // parent class for packs: draw, discard and player
+    private ArrayList<card> cards;
+    public pack(){ cards = new ArrayList<card>(); }
+    public pack(pack p){ cards = new ArrayList<card>(p.cards); }
+    public boolean isEmpty(){ return cards.isEmpty(); }
+    public void shuffle(){ Collections.shuffle(cards); }
+    public void reset(){ cards.clear(); }
+    public void addCard(card c){ cards.add(0, c); }
+    public int size(){ return cards.size(); }
+    public card pickCard(int index, boolean playIt){
+        if (cards.size() <= index) return null;
+        card c = cards.get(index);
+        if (playIt) cards.remove(index); return c;
+    }
+}
+public static class packDraw extends pack{ // draw pack in game 
+    private ArrayList<card> cards;
+    public packDraw(){
+        for (int i = 1; i <= 8; ++i){
+            switch(i){
+                case 8: // 1 of each
+                    cards.add(new card(cardType.SEVEN));
+                    cards.add(new card(cardType.BOMB));
+                    cards.add(new card(cardType.REDEAL));
+                case 7: // 2 of each
+                    cards.add(new card(cardType.SIX));
+                    cards.add(new card(cardType.EQ_10));
+                    cards.add(new card(cardType.DRAW_1));
+                    cards.add(new card(cardType.DRAW_2));
+                case 6: // 3 of each
+                    cards.add(new card(cardType.EQ_0));
+                case 5: // 4 of each
+                    cards.add(new card(cardType.FIVE));
+                    cards.add(new card(cardType.SKIP));
+                    cards.add(new card(cardType.PASS));
+                case 4: // 5 of each
+                    cards.add(new card(cardType.EQ_21));
+                case 3: // 6 of each
+                    cards.add(new card(cardType.REVERSE));
+                case 1: // 8 of each
+                    cards.add(new card(cardType.ZERO));
+                    cards.add(new card(cardType.ONE));
+                    cards.add(new card(cardType.TWO));
+                    cards.add(new card(cardType.THREE));
+                    cards.add(new card(cardType.FOUR));
+                default: continue; // no type has 7 of each
+            }
+        }
+        shuffle();
+    }
+    public packDraw(packDraw p){ super(p); }
+}
+public static class packDiscard extends pack{ // discard pack in game
+    public packDiscard(){ super(); }
+    public packDiscard(packDiscard p){ super(p); }
+}
+public static class packHand extends pack{ // player pack in game
+    public packHand(){ super(); }
+    public packHand(packHand p){ super(p); }
+}
