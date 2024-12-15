cab6e8f89fe1fecddfb2700e705c2064ef3f420b
NhuNguyen0101 <135790490+NhuNguyen0101@users.noreply.github.com>
2024-12-15 14:28:57 +0100

    Update Player.java
    Update method lostLive, playCard, pickCard, getHand

--- Player.java
+++ Player.java
@@ -1,14 +1,16 @@
 import java.util.*;
+import game.card; // Correct import
+
 public class Player{
     private String name;
     private int lives;
     private boolean lost;
-    private packHand hand;
+    private List<Card> hand;
     private String message;
     public Player(String name){
         this.name = name;
         this.lives = 5;
-        this.hand = new packHand();
+        this.hand = new ArrayList<>();
     }
     public String getName() {
         return name;
@@ -18,18 +20,23 @@ public class Player{
         return lives;
     }
 
-    public void loseLife() {
-        if (lives > 0) {
-            lives--;
+    public void lostLive() {
+        this.lives--;
+        if (this.lives <= 0) {
+            System.out.println(name + " has lost all their lives!");
         }
     }
-    public card playCard(int index) {
-        if (index < 0 || index >= hand.size()) {
-            System.out.println("Invalid");
-            return null;
+
+    public void playCard(Card card, int tableValue) {
+        tableValue += card.getValue(); // Add card value to table
+
+        if (tableValue > 21) {
+            lostLive();  // Player loses 1 live
         }
-        return hand.pickCard(index, true);
+
+        hand.remove(card); // Remove card from hand after playing it
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -45,5 +52,11 @@ public class Player{
     }
     public boolean goOut(){
 
+    }
+    public void pickCard(Card card) {
+        hand.add(card);
+    }
+    public List<Card> getHand() {
+        return hand;
     }
 }
