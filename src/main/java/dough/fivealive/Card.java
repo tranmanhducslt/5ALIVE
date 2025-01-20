43eb602cb72c04641d51b661c584c00328c39b95
NhuNguyen0101 <135790490+NhuNguyen0101@users.noreply.github.com>
2025-01-20 12:58:01 +0100

    add method to get card image in Card.java

--- src/main/java/dough/fivealive/Card.java
+++ src/main/java/dough/fivealive/Card.java
@@ -4,6 +4,7 @@
 */
 
 package dough.fivealive;
+import javafx.scene.image.Image;
 
 enum cardType{ // types of cards
     ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, DRAW_1, DRAW_2, PASS, REVERSE, SKIP, EQ_21, EQ_10, EQ_0, REDEAL, BOMB
@@ -13,4 +14,17 @@ public class Card {
     public cardType getCardType(){ return type; }
     public void setCardType(cardType type){ this.type = type; }
     public Card(cardType type){ setCardType(type); }
+    private Image cardImage;
+    private static Image backImage = new Image(Card.class.getResourceAsStream("../resources/img/C-BACK.png"));
+    public Image getImage(){
+        // load card image only when needed
+        if(cardImage == null)
+            cardImage = new Image(
+                Card.class.getResourceAsStream(("../resources/img/C-" + getCardType() + ".png")));
+        return cardImage;
+    }
+
+    public static Image getBackImage(){
+        return backImage;
+    }
 }
