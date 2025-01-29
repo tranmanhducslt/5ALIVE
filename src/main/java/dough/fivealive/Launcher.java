8cfc2076e74f200c6fe46439c4a7c7eff26158ee
tranmanhducslt <79132392+tranmanhducslt@users.noreply.github.com>
2025-01-29 23:56:07 +0100

    add notes pt.2

--- src/main/java/dough/fivealive/Launcher.java
+++ src/main/java/dough/fivealive/Launcher.java
@@ -2,6 +2,6 @@ package dough.fivealive;
 
 public class Launcher {
     public static void main(String[] args){
-        Main.main(args);
+        Main.main(args); // Launch the JavaFX application with Main.java
     }
 }
